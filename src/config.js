8f153fa6967d1598a83016ece37f010f7090dcf1
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2020-07-09 10:50:15 -0400

    Added error logging to Sentry.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
+  env: process.env.NODE_ENV || 'development',
   defaultApiHost: '',
   crisisloggerAPIHost: 'http://localhost:3003'
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import * as Sentry from '@sentry/react';
+
 import * as serviceWorker from './serviceWorker';
 import './i18n';
-import { PersistGate } from 'redux-persist/integration/react'
 import { Store } from './redux/store';
 import App from './App';
 import './index.css';
 import './style/global.scss'
+
+
+// Set up error logging with Sentry.
+if (config.env !== 'development') {
+  Sentry.init({
+    dsn: "https://8cd0d02f082f46c7aaccf908df2617a7@o414302.ingest.sentry.io/5320597",
+    environment: config.env,
+  });
+}
+
 const routing = (
     <ReduxProvider store={Store}>
         <App />
     </ReduxProvider>
-)
+);
 
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(routing, document.getElementById('root'));
 
 serviceWorker.unregister();
